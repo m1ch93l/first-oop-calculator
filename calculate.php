57765ba0597779fb87d50be6e0f62240f201dddd
Michael B. Bañaria <91449532+m1ch93l@users.noreply.github.com>
2026-09-07 16:43:42 +0800

    Revised data

--- calculate.php
+++ calculate.php
@@ -1,9 +1,43 @@
 <?php
 
-include'includes/class-calculate.php';
+class calculateNow
+{
+    public $num1;
+    public $num2;
+    public $calculate;
 
-$num1 = $_POST['num1'];
-$num2 = $_POST['num2'];
+    public function __construct($num1, $num2, $calculate)
+    {
+        $this->num1      = $num1;
+        $this->num2      = $num2;
+        $this->calculate = $calculate;
+    }
+
+    public function calcMethod()
+    {
+        switch ($this->calculate) {
+            case 'addition':
+                $result = $this->num1 + $this->num2;
+                break;
+
+            case 'subtraction':
+                $result = $this->num1 - $this->num2;
+                break;
+
+            case 'multiplication':
+                $result = $this->num1 * $this->num2;
+                break;
+
+            default:
+                $result = "error";
+                break;
+        }
+        return $result;
+    }
+}
+
+$num1      = $_POST['num1'];
+$num2      = $_POST['num2'];
 $calculate = $_POST['calculate'];
 
 $calculator = new calculateNow($num1, $num2, $calculate);
